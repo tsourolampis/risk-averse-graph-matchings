d70eb7cf1e8641781883d3d1bc86d1502c13c408
jlam17 <jlam1618@gmail.com>
2017-12-17 18:04:09 -0500

    adapted to dblp v10

--- dblp_experiments.py
+++ dblp_experiments.py
@@ -8,9 +8,8 @@ def mkdir_subdirec(sub_direc):
     full_path = '{}/{}'.format(abs_path, sub_direc)
     os.makedirs(full_path, exist_ok=True)
 
-def run_experiment(graph, intervals, edge_distrib):
-    # path = 'data/dblp/results/'
-    g = hm.Hypergraph(graph, 'probability', 'citations', distrib=edge_distrib)
+def run_experiment(graph, intervals, edge_distrib,path=None):
+    g = hm.Hypergraph(graph, 'probability', 'weight', distrib=edge_distrib)
     # maximum matching
     _, max_stat = g.max_matching()
     print('Maximum matching')
@@ -21,25 +20,26 @@ def run_experiment(graph, intervals, edge_distrib):
     for idx, beta in enumerate(beta_thresholds):
         bv_matching, bv_stat = g.bounded_var_matching(beta, edge_distrib)
         bv_results.append(bv_stat)
-        # f = path + 'bv_matchings-{}.pkl'.format(idx)
-        # pickle.dump(bv_matching, open(f, 'wb'))
+        if path is not None:
+            f = path + 'bv_matchings-{}.pkl'.format(idx)
+            pickle.dump(bv_matching, open(f, 'wb'))
         g.print_stats(bv_stat, beta)
     return max_stat, bv_results
 
 def main():
     path = 'data/dblp'
     print('Loading in dblp data...')
-    f = '{}/{}'.format(path, 'dblp_graph.pkl')
+    f = '{}/{}'.format(path, 'dblp_v10_graph_final.pkl')
     graph = pickle.load( open(f, 'rb'))
     print('Starting experiment on {} graph with {} hyperedges'.format(f, len(graph)))
 
     start = time.time()
     edge_distrib = 'bernoulli'
     intervals = 20
-    max_stats, bv_stats = run_experiment(graph, intervals, edge_distrib)
-    print('Finished finding bounded variance matchings')
-    path = path + '/results/'
+    path = path + '/results_v10/'
     mkdir_subdirec(path)
+    max_stats, bv_stats = run_experiment(graph, intervals, edge_distrib, path)
+    print('Finished finding bounded variance matchings')
     f = path + 'max_stats.pkl'
     pickle.dump(max_stats, open(f, 'wb'))
     f = path + 'bv_stats.pkl'
